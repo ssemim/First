87a7288e3152cb35e63a52f04650c541ecb3f951
ssemim <jhl2_014@naver.com>
2023-05-24 16:17:11 +0900

    세번쨰 커밋

--- vcs Test/src/Hello.java
+++ vcs Test/src/Hello.java
@@ -6,6 +6,9 @@ public class Hello {
 		System.out.println("Hello World!");
 		
 		System.out.println("변경");
+		System.out.println("세 번째 커밋");
+		//내 저장소 : 로컬 리포지토리
+		//원격저장소 : 리모트 리포지토리(깃허브)
 		
 	}
 
